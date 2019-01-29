446c91fd2394ccb0d367892248958f297f9c3d60
杨玉梅 <305883594@qq.com>
2019-01-29 23:00:23 +0800

    .

---
+++ main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// import 'lib-flexible'
+// import '@/styles/index.scss'
+// import Vue from 'vue'
+import 'swiper/dist/css/swiper.css';
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
